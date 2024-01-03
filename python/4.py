eaf2bac8ba4d5835f213fa66ca25f3fac9a7bfbb
Adityacodin <mohite.aditya.comp@kgce.edu.in>
2024-01-03 22:01:31 +0530

    python syntax basics 1

---
+++ python/4.py
@@ -0,0 +1,16 @@
+print("hello world")
+# /\n for a newline
+print("hello","world",1+15, "it is a great \n pleasure")
+# single line comment
+
+# mulltiline comment
+'''
+this is multiline comment
+'''
+
+"""
+this is 1st comment
+this is 2nd line
+
+"""
+print("hello", 6 ,7 ,sep="$", end="aditya\n")
